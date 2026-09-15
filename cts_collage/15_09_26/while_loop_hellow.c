199959e04825e386deb4107c0e5bbd70a7953581
Suraj Kumar <surajkm898@gmail.com>
2026-09-15 17:41:01 +0530

    update

---
+++ cts_collage/15_09_26/while_loop_hellow.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+    int i =1;
+    while (i<=0)
+    {
+       printf("%d",i);
+       i++;
+    }
+    do
+    {
+         printf("%d",i);
+       i++;
+    } while (i<=0);
+    
+}
