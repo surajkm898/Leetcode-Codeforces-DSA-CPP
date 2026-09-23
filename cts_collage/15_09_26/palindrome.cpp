8577630b68ad8ce9c32de03d92203953ec89d1cc
Suraj Kumar <surajkm898@gmail.com>
2026-09-23 15:14:37 +0530

    update

--- cts_collage/15_09_26/palindrome.cpp
+++ cts_collage/15_09_26/palindrome.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main (){
     int n = 5734;
     int m = 0;
-    int k;
+    int k ;
     for (int i=1;0<n;i++){
-        k = (n%10);
-        n = n/10;
-        k += m;
-        m = *10;
+        m *= 10;
+       m += n%10;
+       n = n/10;
     }
+    cout <<m;
 }
